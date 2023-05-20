f38fbd3262d2feb3d73cd34b973aa8f135cc830a
Sawalon <sawalon.chen@ui.com>
2023-05-20 21:13:58 +0800

    feat: SwiftUI Text

---
+++ SwiftUIKit/SwiftUIKit/Image/SwiftUIImageView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIImageView.swift
+//  SwiftUIKit
+//
+//  Created by sawalon.chen on 2023/5/20.
+//
+
+import SwiftUI
+
+struct SwiftUIImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIImageView()
+    }
+}
---
+++ SwiftUIKit/SwiftUIKit/Text/SwiftUITextView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUITextView.swift
+//  SwiftUIKit
+//
+//  Created by sawalon.chen on 2023/5/20.
+//
+
+import SwiftUI
+
+struct SwiftUITextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUITextView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUITextView()
+    }
+}
