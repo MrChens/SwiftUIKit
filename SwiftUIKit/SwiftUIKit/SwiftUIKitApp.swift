430db804e7a663be12bd1602156c98cf86d106cb
Sawalon <sawalon.chen@ui.com>
2023-05-20 14:34:33 +0800

    chore: init proj

---
+++ SwiftUIKit/SwiftUIKit/SwiftUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKitApp.swift
+//  SwiftUIKit
+//
+//  Created by sawalon.chen on 2023/5/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
