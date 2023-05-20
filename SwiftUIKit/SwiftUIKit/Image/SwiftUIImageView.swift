afcfff0c5f83bfb8870035ebafb913c224691ace
Sawalon <sawalon.chen@ui.com>
2023-05-20 21:14:20 +0800

    feat: SwiftUI Image

--- SwiftUIKit/SwiftUIKit/Image/SwiftUIImageView.swift
+++ SwiftUIKit/SwiftUIKit/Image/SwiftUIImageView.swift
@@ -9,7 +9,94 @@ import SwiftUI
 
 struct SwiftUIImageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image(systemName: "cloud.heavyrain")
+                    .font(.system(size: 50))
+                    .foregroundColor(.blue)
+                    .shadow(color: .gray, radius: 10, x: 0, y: 10)
+                    .overlay(alignment: .center, content: {
+                        Image(systemName: "heart.fill")
+                            .font(.system(size: 50))
+                            .foregroundColor(.black)
+                            .opacity(0.5)
+                            .overlay(content: {
+                                Text("Rain")
+                                    .fontWeight(.black)
+                                    .foregroundColor(.white)
+                            })
+                    })
+                    .padding(4)
+
+                Image(systemName: "cloud.sun.rain")
+                    .font(.system(size: 50))
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.blue, .yellow, .gray)
+                    .padding(4)
+                
+                Image(systemName: "slowmo", variableValue: 0.5)
+                    .symbolRenderingMode(.palette)
+                    .foregroundColor(.green)
+                    .font(.system(size: 50))
+                    .padding(4)
+                
+            }
+            
+            Image("paris")
+                .resizable()
+//                .scaledToFit()
+//                .aspectRatio( contentMode: .fit)
+                .aspectRatio( contentMode: .fill)
+                .frame(width: 300)
+//                .clipped()
+//                .clipShape(Circle())
+//                .ignoresSafeArea(.container, edges: .bottom)
+//                .opacity(0.5)//不透明度，0 完全透明，1 完全不透明
+                .overlay(content: {
+                    Color.black
+                        .opacity(0.4)
+                })
+                .overlay(alignment: .top, content: {
+                    Text("If you are lucky enough to have lived in Paris as a young man, then wherever you go for the rest of your life it stays with you, for Paris is a moveable feast.\n\n- Ernest Hemingway")
+                        .fontWeight(.heavy)
+                        .font(.system(.headline, design: .rounded))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(10)
+                        .opacity(0.8)
+                        .padding()
+                })
+        }
+        
+//        Shadow()
+    }
+}
+
+struct Shadow: View {
+    let steps = [0, 5, 10]
+
+    var body: some View {
+        VStack(spacing: 50) {
+            ForEach(steps, id: \.self) { offset in
+                HStack(spacing: 50) {
+                    ForEach(steps, id: \.self) { radius in
+                        Color.blue
+                            .shadow(
+                                color: .primary,
+                                radius: CGFloat(radius),
+                                x: CGFloat(offset), y: CGFloat(offset))
+                            .overlay {
+                                VStack {
+                                    Text("\(radius)")
+                                    Text("(\(offset), \(offset))")
+                                }
+                            }
+                    }
+                }
+            }
+        }
+        .padding()
     }
 }
 
--- SwiftUIKit/SwiftUIKit/Text/SwiftUITextView.swift
+++ SwiftUIKit/SwiftUIKit/Text/SwiftUITextView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct SwiftUITextView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+                .padding(4)
+
+            Text("**This is how you bold a text**. *This is how you make text italic.* You can [click this link](https://www.appcoda.com) to go to appcoda.com")
+                .fontWeight(.bold)
+//                .font(.title)
+//                .font(.system(
+//                    size: 14,
+//                    weight: .regular,
+//                    design: .default)
+//                )
+                .font(.custom("Nunito", size: 12)) // 自定义字体
+                .padding(4)
+
+            Text("Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+                .font(.body)
+
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .lineLimit(10)
+//                .lineLimit(nil)// nil 支持多行，默认为nil
+                .truncationMode(.tail)
+                .lineSpacing(8.0)
+                .padding(16)
+//                .rotationEffect(.degrees(4))//默认，旋转动作会以文字视图为中心来旋转
+//                .rotationEffect(.degrees(4), anchor: .topLeading)
+//                .rotation3DEffect(
+//                    .degrees(35),
+//                    axis: (x: 1, y: 0, z: 0)
+//                )
+                .shadow(color: .gray, radius: 2, x: 4, y: 4) // 颜色，阴影模糊等级，0不模糊，越大越模糊，x，y的偏移
+            // markdown 格式
+        }
+        .padding()
     }
 }
 
